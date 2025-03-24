f40e8c1c8bf9025111dcc87cb00150b26257e32a
graziella.pereira <graziellapereira52@gmail.com>
2025-03-24 17:52:48 -0300

    continuar exercicio 3

---
+++ Exercicio3/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3;
+public class Produto
+{
+    public string nome;
+    public double preco;
+    public int quantidade, estoque;
+
+    public void AdicionarEstoque()
+    {
+        Console.WriteLine($"Digite a quantidade de unidades de {nome} que serão adicionadas ao estoque: ");
+        quantidade = int.Parse(Console.ReadLine());
+        Console.WriteLine($"{quantidade} unidades de {nome} adicionados(as) ao estoque.");
+        estoque += quantidade;
+    }
+    public void Re
+}
